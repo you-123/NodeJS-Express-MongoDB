7f14cd684e559a80a314bdddaa8955226e2e3e59
yusuf <yesufbushira9@gmail.com>
2020-10-17 13:11:09 -0500

    Week 1 Workshop Assignment

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
-const  bodyParser=require('body-parser');
+const bodyParser = require('body-parser');
 const campsiteRouter = require('./routes/campsiteRouter');
-
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use('/campsites',campsiteRouter);
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners',partnerRouter);
 app.use(express.static(__dirname + '/public'));
-app.use((req, res) => { 
+app.use((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
